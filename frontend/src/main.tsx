a62845b68be19d1df9d9115da15f66360557c45d
Yhuver Quintero <yhuver@gmail.com>
2025-04-23 11:33:44 -0500

    refactor: move providers to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {ThemeProvider} from "@/providers/theme-provider.tsx";
+import {AuthProvider} from "@/contexts/AuthContext.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <App/>
+            <AuthProvider>
+                <App/>
+            </AuthProvider>
         </ThemeProvider>
-    </StrictMode>,
-)
+    </StrictMode>
+);
