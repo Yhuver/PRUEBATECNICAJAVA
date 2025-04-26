ca6fcab7561e2d3a756eceffb1560b3b454054c1
Yhuver Quintero <yhuver@gmail.com>
2025-04-25 22:38:24 -0500

    refactor: remove Strict mode to solve unecessary request to back

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,13 @@
-import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {ThemeProvider} from "@/providers/theme-provider.tsx";
 import {AuthProvider} from "@/contexts/AuthContext.tsx";
 
-
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <AuthProvider>
-                <App/>
-            </AuthProvider>
-        </ThemeProvider>
-    </StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <AuthProvider>
+            <App/>
+        </AuthProvider>
+    </ThemeProvider>
 );
